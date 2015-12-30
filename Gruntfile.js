19c39c711333128979d20ea1ddf73688ecd15cb3
Hack Reactor Students <accounts@hackreactor.com>
2015-12-30 15:45:23 -0800

    Added jshint to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -108,7 +108,7 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('deploy', [ 'concat']);
+  grunt.registerTask('deploy', [ 'concat', 'jshint']);
   //, 'uglify', 'jshint', 'mochaTest'
 
 
