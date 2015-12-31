de587b03ad614e99509dea176ebe343187ee3111
Hack Reactor Students <accounts@hackreactor.com>
2015-12-30 19:16:58 -0800

    First pass at refactoring for MongoDB

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,33 @@
-var db = require('../config');
+var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+var userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  timestamps: { type: Date, default: Date.now }
 });
+
+userSchema.pre('save', function(next) {
+  this.hashPassword();
+  next();
+})
+
+userSchema.methods.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    callback(isMatch);
+  });
+};
+
+userSchema.methods.hashPassword = function() {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      // this.set('password', hash);
+  });
+};
+
+var User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 var app = require('./server-config.js');
+var mongo = require('mongdb');
+var mongoose = require('mongoose');
 
-var port = process.env.PORT;
+var port = process.env.PORT || 4568;
 
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
+
+var uristring =
+  process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/HelloMongoose';
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
